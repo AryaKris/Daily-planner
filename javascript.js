60881680e9aab62e0b966a4720941e4083a220c6
Arya Krishna <dr.aryakris@outlook.com>
2021-11-30 22:13:54 -0800

    save button updates

--- javascript.js
+++ javascript.js
@@ -13,4 +13,10 @@ if (timeBlock<hour){
     $(this).children(".description").addClass ("future")
 }
 });
+$(".saveBtn").each (function (index){
+    $(this).on("click", function (event){
+console.log ($(this).siblings(".description").val())
+    })
+
+});
 
